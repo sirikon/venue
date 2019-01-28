3ea9d4fcd6040bb8ef32b2c41c839ca93aaa4e61
Carlos Fernández Llamas <hello@sirikon.me>
2019-01-28 21:19:33 +0100

    Add missing data talks file and CircleCi configuration

---
+++ test/data/talks.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        name: 'Cool talk',
+        slug: 'cool-talk',
+        description: 'General coolness',
+        speaker: 'John Doe',
+        speakerImage: 'jdoe.jpg',
+        speakerTitle: 'You know him',
+        track: '1',
+        when: 'VIE 9:00-10:00'
+    },
+    {
+        name: 'Another cool talk',
+        slug: 'another-cool-talk',
+        description: 'Amazingness',
+        speaker: 'Dohn Joe',
+        speakerImage: 'djoe.jpg',
+        speakerTitle: 'You know him too',
+        track: '1',
+        when: 'VIE 10:00-11:00'
+    }
+];
