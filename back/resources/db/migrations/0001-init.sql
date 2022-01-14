811913df47f110a721db80a9700cbb09c628f066
Carlos Fdez. Llamas <hello@sirikon.me>
2022-01-14 01:19:40 +0100

    add tables

--- back/resources/db/migrations/0001-init.sql
+++ back/resources/db/migrations/0001-init.sql
@@ -8,3 +8,17 @@ CREATE TABLE talks (
   track VARCHAR NOT NULL,
   date TIMESTAMP WITH TIME ZONE
 );
+
+CREATE TABLE questions (
+  visitor_id UUID NOT NULL,
+  talk_id INTEGER REFERENCES talks (id),
+  question VARCHAR NOT NULL
+);
+
+CREATE TABLE ratings (
+  visitor_id UUID NOT NULL,
+  talk_id INTEGER REFERENCES talks (id),
+  rating SMALLINT NOT NULL,
+  comment VARCHAR,
+  UNIQUE (visitor_id, talk_id)
+);
