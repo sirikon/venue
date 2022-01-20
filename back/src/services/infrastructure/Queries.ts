cbdbaeac196285c036d46c42414950ed5a3c70bc
Carlos Fdez. Llamas <hello@sirikon.me>
2022-01-20 01:25:35 +0100

    New query

--- back/src/services/infrastructure/Queries.ts
+++ back/src/services/infrastructure/Queries.ts
@@ -1,4 +1,4 @@
-import { Talk } from "../../models/mod.ts";
+import { Question, Talk } from "../../models/mod.ts";
 import { withClient, WithClientFunc } from "../external/database.ts";
 
 export class Queries {
@@ -13,6 +13,17 @@ export class Queries {
           id, slug, name, description, speaker_name, speaker_title, speaker_image, track, date, r.rating as rated
         FROM talks t
         LEFT JOIN ratings r on r.talk_id = t.id and r.visitor_id = ${visitorId};`
+        .then((r) => r.rows);
+    });
+  }
+
+  talkQuestions(talk: Pick<Talk, "id">) {
+    return this.withClient(async (client) => {
+      return await client.queryObject<Question>`
+        SELECT
+          id, visitor_id, talk_id, question
+        FROM questions
+        WHERE talk_id = ${talk.id};`
         .then((r) => r.rows);
     });
   }
