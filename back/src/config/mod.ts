b2791a4065e4083f3e0ab4fec17bfc9f1ed538d2
Carlos Fdez. Llamas <hello@sirikon.me>
2022-01-26 02:21:21 +0100

    change config

--- back/src/config/mod.ts
+++ back/src/config/mod.ts
@@ -1,50 +1,68 @@
+import { buildConfig } from "../vendor/denox/config/environment.ts";
+
 const isTrue = (v: string) => v === "true";
 
-const getEnv = <
-  KT extends string,
-  VT = string,
-  ReqT extends boolean = true,
->(
-  key: KT,
-  opts?: {
-    required?: ReqT;
-    default?: string;
-    map?: (value: string) => VT;
+const config = buildConfig({
+  VENUE_PORT: {
+    description: "Port to listen to",
+    default: "8000",
+    map: parseInt,
   },
-): { [key in KT]: (ReqT extends true ? VT : (VT | null)) } => {
-  const value = Deno.env.get(key);
-  const mapper = opts?.map || ((v: string) => v);
+  VENUE_DB_USER: {
+    description: "PostgreSQL username",
+  },
+  VENUE_DB_PASSWORD: {
+    description: "PostgreSQL password",
+  },
+  VENUE_DB_NAME: {
+    description: "PostgreSQL database name",
+  },
+  VENUE_DB_HOST: {
+    description: "PostgreSQL database host",
+  },
+  VENUE_DB_PORT: {
+    description: "PostgreSQL database port",
+    default: "5432",
+    map: parseInt,
+  },
+  VENUE_DB_POOL_SIZE: {
+    description: "PostgreSQL connection pool size",
+    default: "5",
+    map: parseInt,
+  },
+  VENUE_TEMPLATE_CACHING: {
+    description: "Enable template caching",
+    default: "false",
+    map: isTrue,
+  },
+  VENUE_THEMES_FOLDER: {
+    description: "Location of themes folder",
+    default: "resources/themes",
+  },
+  VENUE_THEME_NAME: {
+    description: "Theme name",
+    default: "default",
+  },
+  VENUE_VISITOR_COOKIE_SECRET: {
+    description: "The secret to be used when signing cookies",
+  },
+  VENUE_BRAND_NAME: {
+    description: "Brand name",
+  },
+  VENUE_BRAND_IMAGE: {
+    description: "Absolute URL to the brand image",
+    required: false,
+  },
+  VENUE_ADMIN_USERNAME: {
+    description: "Administrator username",
+  },
+  VENUE_ADMIN_PASSWORD: {
+    description: "Administrator password",
+  },
+  VENUE_EXTRA_META: {
+    description: "Extra meta elements to be added on top of <head>",
+    required: false,
+  },
+});
 
-  if (value) {
-    return { [key]: mapper(value) } as { [key in KT]: VT };
-  }
-  if (opts?.default) {
-    return { [key]: mapper(opts.default) } as { [key in KT]: VT };
-  }
-  if (opts == null || opts.required == null || opts.required === true) {
-    throw new Error(`Missing required environment variable: ${key}`);
-  }
-  return { [key]: null } as {
-    [key in KT]: (ReqT extends true ? VT : (VT | null));
-  };
-};
-
-const PREFIX = "VENUE";
-export default {
-  ...getEnv(`${PREFIX}_PORT`, { default: "8000", map: parseInt }),
-  ...getEnv(`${PREFIX}_DB_USER`),
-  ...getEnv(`${PREFIX}_DB_PASSWORD`),
-  ...getEnv(`${PREFIX}_DB_NAME`),
-  ...getEnv(`${PREFIX}_DB_HOST`),
-  ...getEnv(`${PREFIX}_DB_PORT`, { default: "5432", map: parseInt }),
-  ...getEnv(`${PREFIX}_DB_POOL_SIZE`, { default: "5", map: parseInt }),
-  ...getEnv(`${PREFIX}_TEMPLATE_CACHING`, { default: "false", map: isTrue }),
-  ...getEnv(`${PREFIX}_THEMES_FOLDER`, { default: "resources/themes" }),
-  ...getEnv(`${PREFIX}_THEME_NAME`, { default: "default" }),
-  ...getEnv(`${PREFIX}_VISITOR_COOKIE_SECRET`),
-  ...getEnv(`${PREFIX}_BRAND_NAME`),
-  ...getEnv(`${PREFIX}_BRAND_IMAGE`, { required: false }),
-  ...getEnv(`${PREFIX}_ADMIN_USERNAME`),
-  ...getEnv(`${PREFIX}_ADMIN_PASSWORD`),
-  ...getEnv(`${PREFIX}_EXTRA_META`, { required: false }),
-};
+export default config.read();
