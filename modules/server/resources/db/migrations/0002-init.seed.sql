ec807cbd798d09f7b06d43a9279d60a7cc2e1be3
Carlos Fdez. Llamas <hello@sirikon.me>
2023-01-18 20:25:06 +0100

    Descriptions can (should) include HTML

--- modules/server/resources/db/migrations/0002-init.seed.sql
+++ modules/server/resources/db/migrations/0002-init.seed.sql
@@ -1,13 +1,13 @@
 INSERT INTO talks (slug, name, description, speaker_name, speaker_title, speaker_image, track, date) VALUES
   (
     'the-mother-of-all-demos', 'The Mother of All Demos',
-    'The live demonstration featured the introduction of a complete computer hardware and software system called the oN-Line System or, more commonly, NLS. The 90-minute presentation demonstrated for the first time many of the fundamental elements of modern personal computing: windows, hypertext, graphics, efficient navigation and command input, video conferencing, the computer mouse, word processing, dynamic file linking, revision control, and a collaborative real-time editor. Engelbart presentation was the first to publicly demonstrate all of these elements in a single system. The demonstration was highly influential and spawned similar projects at Xerox PARC in the early 1970s. The underlying concepts and technologies influenced both the Apple Macintosh and Microsoft Windows graphical user interface operating systems in the 1980s and 1990s.',
+    '<p>The live demonstration featured the introduction of a complete computer hardware and software system called the oN-Line System or, more commonly, NLS.</p><p>The 90-minute presentation demonstrated for the first time many of the fundamental elements of modern personal computing: windows, hypertext, graphics, efficient navigation and command input, video conferencing, the computer mouse, word processing, dynamic file linking, revision control, and a collaborative real-time editor.</p><p>Engelbart presentation was the first to publicly demonstrate all of these elements in a single system. The demonstration was highly influential and spawned similar projects at Xerox PARC in the early 1970s. The underlying concepts and technologies influenced both the Apple Macintosh and Microsoft Windows graphical user interface operating systems in the 1980s and 1990s.</p>',
    'Douglas Engelbart', 'Engineer. Inventor. Internet pioneer.', 'http://localhost:8001/engelbart.jpg',
    'Track 1', '2022-01-20 09:00:00 UTC'
   ),
   (
     'a-whole-new-world', 'A Whole New World',
-    'This talk announces the most ambitious software project I have ever undertaken, then considers why its existence is so surprising (and in some cases frustrating) to people. ',
+    '<p>This talk announces the most ambitious software project I have ever undertaken, then considers why its existence is so surprising (and in some cases frustrating) to people.</p>',
     'Gary Bernhardt', 'Destroy All Software', 'http://localhost:8001/bernhardt.jpg',
     'Track 1', '2022-01-20 10:00:00 UTC'
   )
