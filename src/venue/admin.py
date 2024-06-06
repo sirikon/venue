3f337473036d4c460adff313e134e740cc587f23
Carlos Fdez. Llamas <hello@sirikon.me>
2024-06-07 01:08:16 +0200

    wip

--- src/venue/admin.py
+++ src/venue/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Talk, Speaker
+from .models import Talk, Speaker, Track
 
 admin.site.register(Talk)
 admin.site.register(Speaker)
+admin.site.register(Track)
--- src/venue/models.py
+++ src/venue/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Talk(models.Model):
     name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
     description = models.TextField()
     date = models.DateTimeField()
 
@@ -14,3 +15,13 @@ class Speaker(models.Model):
     name = models.CharField(max_length=200)
     title = models.TextField()
     image = models.ImageField(upload_to="speakers")
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Track(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
