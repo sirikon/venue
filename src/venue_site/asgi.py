0df5f4d9631d318ca696c63169b072a7c623021b
Carlos Fdez. Llamas <hello@sirikon.me>
2024-06-07 00:36:20 +0200

    wip

--- src/venue_site/asgi.py
+++ src/venue_site/asgi.py
@@ -1,11 +1,4 @@
-"""
-ASGI config for venue project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
+# https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
--- src/venue_site/wsgi.py
+++ src/venue_site/wsgi.py
@@ -1,11 +1,4 @@
-"""
-WSGI config for venue project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
+# https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
