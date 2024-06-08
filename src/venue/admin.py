afce5d6c972bebfe8996ab8b7defbc4b29d2c8b8
Carlos Fdez. Llamas <hello@sirikon.me>
2024-06-08 03:29:46 +0200

    wip

--- src/venue/admin.py
+++ src/venue/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Talk, Speaker, Track
+from .models import Talk, Speaker, Track, Rating, Question
 
 admin.site.register(Talk)
 admin.site.register(Speaker)
 admin.site.register(Track)
+admin.site.register(Rating)
+admin.site.register(Question)
--- src/venue/models.py
+++ src/venue/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 
 
-class Talk(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200)
-    description = models.TextField()
-    date = models.DateTimeField()
+class Track(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self) -> str:
         return self.name
@@ -20,8 +17,26 @@ class Speaker(models.Model):
         return self.name
 
 
-class Track(models.Model):
-    name = models.CharField(max_length=100)
+class Talk(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    description = models.TextField(blank=True)
+    date = models.DateTimeField()
+    speakers = models.ManyToManyField(Speaker)
+    track = models.ForeignKey(Track, on_delete=models.RESTRICT)
 
     def __str__(self) -> str:
         return self.name
+
+
+class Question(models.Model):
+    talk = models.ForeignKey(Talk, on_delete=models.CASCADE)
+    visitor = models.UUIDField()
+    question = models.TextField()
+
+
+class Rating(models.Model):
+    talk = models.ForeignKey(Talk, on_delete=models.CASCADE)
+    visitor = models.UUIDField()
+    rating = models.PositiveSmallIntegerField()
+    comment = models.TextField()
