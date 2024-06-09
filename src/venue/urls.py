6d18a1b86e9e1d531c2254871a5a2fe269298335
Carlos Fdez. Llamas <hello@sirikon.me>
2024-06-09 21:30:52 +0200

    wip

--- src/venue/urls.py
+++ src/venue/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("talk/<str:slug>", views.talk, name="talk"),
+    path("talk/<str:slug>/question", views.talk_question, name="talk_question"),
+    path("talk/<str:slug>/rating", views.talk_rating, name="talk_rating"),
 ]
--- src/venue/views.py
+++ src/venue/views.py
@@ -9,6 +9,19 @@ def index(request):
     talks = (
         Talk.objects.all()
         .annotate(speakers_names=ArrayAgg("speakers__name"))
-        .values("name", "date", "track__name", "speakers_names")
+        .values("name", "slug", "date", "track__name", "speakers_names")
     )
     return render(request, "venue/index.html", {"talks": talks})
+
+
+def talk(request, slug):
+    talk = Talk.objects.filter(slug=slug).first()
+    return render(request, "venue/talk.html", {"talk": talk})
+
+
+def talk_question(request):
+    pass
+
+
+def talk_rating(request):
+    pass
