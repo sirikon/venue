e6696fe7c206c6c54e99aa0dd74762d19dc37880
Carlos Fdez. Llamas <hello@sirikon.me>
2024-06-09 21:08:32 +0200

    wip

---
+++ src/venue/context_processors.py
@@ -0,0 +1,2 @@
+def venue_context_processor(_):
+    return {"BRAND_NAME": "Changeme 2024"}
--- src/venue/views.py
+++ src/venue/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.postgres.aggregates import ArrayAgg
+
+from venue.models import Talk
 
 
 def index(request):
-    return render(request, "venue/index.html")
+    talks = (
+        Talk.objects.all()
+        .annotate(speakers_names=ArrayAgg("speakers__name"))
+        .values("name", "date", "track__name", "speakers_names")
+    )
+    return render(request, "venue/index.html", {"talks": talks})
