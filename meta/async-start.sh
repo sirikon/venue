b563062efe1a86de5546a8357969400c1225c3e4
Carlos Fdez. Llamas <hello@sirikon.me>
2024-10-09 20:27:56 +0200

    Remove async stuff. Django templates can't be async and that's a pain that I will dodge for now.

--- meta/async-start.sh
+++ meta/async-start.sh
@@ -4,6 +4,5 @@ set -euo pipefail
 root="$(pwd)"
 export PYTHONPATH="$root/src"
 exec "$root/.venv/bin/python" -m gunicorn \
-  venue_site.asgi:application \
-  --workers 1 \
-  --worker-class uvicorn.workers.UvicornWorker
+  venue_site.wsgi:application \
+  --workers 1
