1f59c31770c0124afcf0b9ada2e49a5538705fc7
Carlos Fdez. Llamas <hello@sirikon.me>
2024-12-10 19:45:42 +0100

    preload on gunicorn start

--- meta/gunicorn-start.sh
+++ meta/gunicorn-start.sh
@@ -8,4 +8,5 @@ export VENUE_DEVENV_FIXTURES="$root/meta/_devenv/fixtures"
 export PYTHONPATH="$root/src"
 exec "$root/.venv/bin/python" -m gunicorn \
   venue_site.wsgi:application \
+  --preload \
   --workers 1
