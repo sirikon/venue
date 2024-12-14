66b697ff988313950703048262211b94905760e4
Carlos Fdez. Llamas <hello@sirikon.me>
2024-12-14 18:11:24 +0100

    wip

--- meta/docker/start.sh
+++ meta/docker/start.sh
@@ -2,4 +2,10 @@
 set -euo pipefail
 
 "$(dirname "${BASH_SOURCE[0]}")/build.sh"
-docker run -it --rm --publish 127.0.0.1:8000:80 venue:dev "$@"
+docker run \
+  -it --rm \
+  --publish 127.0.0.1:8000:80 \
+  -e VENUE_DEBUG=true \
+  -e VENUE_PORT_PREFIX=80 \
+  --network host \
+  venue:dev "$@"
