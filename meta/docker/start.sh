d86f223f7b24e461facabd58347f88a9d18c00fc
Carlos Fdez. Llamas <hello@sirikon.me>
2025-07-31 22:24:42 +0200

    perf testing scripts

--- meta/docker/start.sh
+++ meta/docker/start.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 "$(dirname "${BASH_SOURCE[0]}")/build.sh"
 docker run \
   -it --rm \
-  -e VENUE_WORKERS=4 \
-  -e VENUE_THREADS=4 \
+  -e "VENUE_WORKERS=${VENUE_WORKERS:-}" \
+  -e "VENUE_THREADS=${VENUE_THREADS:-}" \
   -e VENUE_SECRET_KEY=secret-secret-secret-secret-secret-secret-secret-secret \
   -e VENUE_DISABLE_SECURE_COOKIES=true \
   -e VENUE_PORT_PREFIX=80 \
--- tests/perf/src/test.ts
+++ tests/perf/src/test.ts
@@ -1,5 +1,14 @@
+import { check } from "k6";
 import http from "k6/http";
 
+export const options = {
+  vus: 1000,
+  duration: "10s",
+};
+
 export default function () {
-  http.get("http://127.0.0.1:8080");
+  const res = http.get("http://127.0.0.1:8080/talk/the-mother-of-all-demos");
+  check(res, {
+    "is status 200": (r) => r.status === 200,
+  });
 }
