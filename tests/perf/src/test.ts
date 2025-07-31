4504ac7d1617a334b13bff68f06651d483cb2726
Carlos Fdez. Llamas <hello@sirikon.me>
2025-07-31 22:26:40 +0200

    parameterize perf test address

--- tests/perf/src/test.ts
+++ tests/perf/src/test.ts
@@ -1,13 +1,15 @@
 import { check } from "k6";
 import http from "k6/http";
 
+const VENUE_ADDRESS = __ENV.VENUE_ADDRESS || "http://127.0.0.1:8080";
+
 export const options = {
   vus: 1000,
   duration: "10s",
 };
 
 export default function () {
-  const res = http.get("http://127.0.0.1:8080/talk/the-mother-of-all-demos");
+  const res = http.get(`${VENUE_ADDRESS}/talk/the-mother-of-all-demos`);
   check(res, {
     "is status 200": (r) => r.status === 200,
   });
