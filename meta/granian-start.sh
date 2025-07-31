e14c0b5d82569109c0fabfc788b5b4f1cb675f2b
Carlos Fdez. Llamas <hello@sirikon.me>
2025-07-31 23:18:52 +0200

    granian start script

--- meta/granian-start.sh
+++ meta/granian-start.sh
@@ -8,8 +8,10 @@ mkdir -p .workdir
 cd .workdir
 
 export PYTHONPATH="$VENUE_ROOT/src"
-exec "$VENUE_ROOT/.venv/bin/python" -m gunicorn \
+exec "$VENUE_ROOT/.venv/bin/granian" \
+  --interface wsgi \
   venue_django.wsgi:application \
-  --preload \
-  --workers 1 \
-  --bind 127.0.0.1:8080
+  --host="127.0.0.1" \
+  --port="8080" \
+  --workers 10 \
+  --blocking-threads 10
